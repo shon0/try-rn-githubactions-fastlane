ded7a09d052d9e7d40b16a5b9aebe867d6dfc64c
shon0 <shono.koga@gmail.com>
2021-05-20 14:45:35 +0900

    Add create pull request script

---
+++ .github/script/create_pull_request.js
@@ -0,0 +1,35 @@
+const simpleGit = require('simple-git');
+
+module.exports = async ({github}) => {
+  //   const git = simpleGit(path);
+  //   const logs = await git.tags({'--sort': '-v:refname'}).then(t => {
+  //     const lastTag = t.all[(t, all.length)];
+  //     return git.log({from: lastTag});
+  //   });
+
+  //   console.log(logs);
+
+  const {data} = await github.pulls.list({
+    owner: 'sumashin',
+    repo: 'tsukuyomi',
+    base: 'master',
+    head: process.env.HEAD_BRANCH,
+    state: 'closed',
+  });
+
+  const res = data.map(d => {
+    return {
+      title: d.title,
+      url: d.html_url,
+      number: d.number,
+      merge_commit_sha: d.merge_commit_sha,
+    };
+  });
+
+  console.log(res);
+
+  //   return res
+  //     .filter(d => logs.all.some(l => l.hash === d.merge_commit_sha))
+  //     .map(pr => `- [#${pr.number}](${pr.url}) ${pr.title}`)
+  //     .join('\n');
+};
