11843a69f8bf29928d6531e678ede6f4b2ca7a25
shon0 <shono.koga@gmail.com>
2021-05-24 12:44:20 +0900

    Add log

--- .github/script/create-release-pull-request.js
+++ .github/script/create-release-pull-request.js
@@ -1,31 +1,47 @@
 module.exports = async ({github, repository, head}) => {
   const [owner, repo] = repository.split('/');
   const [, version] = head.split('/');
+  const commits = getCommit(github, {owner, repo, head});
+  const pullList = getPullList(github, {owner, repo, head});
 
-  // masterとheadの差分となるcommitを取得 (ページングしない場合はレスポンスは最大250件)
-  const compareCommitsResult = await github.repos.compareCommits({
+  const body = createPullRequestBody(pullList, commits);
+
+  createPullRequest(github, {
+    owner,
+    repo,
+    head,
+    body,
+    version,
+  });
+};
+
+// masterとheadの差分となるcommitを取得 (ページングしない場合はレスポンスは最大250件)
+const getCommit = (github, {owner, repo, head}) => {
+  const result = await github.repos.compareCommits({
     owner,
     repo,
     head,
     base: 'master',
     state: 'closed',
   });
-
   // 取得した差分からshaだけの配列を作成
-  const commits = compareCommitsResult.data.commits.map(commit => ({
+  const commits = result.data.commits.map(commit => ({
     sha: commit.sha,
   }));
 
+  return commits;
+};
+
+const getPullList = (github, {owner, repo, head}) => {
   // headをベースに作成されたPR一覧を取得 (1ページあたりの結果（最大100）)
-  const pullsListResult = await github.pulls.list({
+  const result = await github.pulls.list({
     owner,
     repo,
     base: head,
     state: 'closed',
   });
-
   // 取得したPR一覧を必要な値のみの配列に変換
-  const pullsList = pullsListResult.data.map(d => {
+  const pullList = result.data.map(d => {
     return {
       title: d.title,
       number: d.number,
@@ -34,9 +50,38 @@ module.exports = async ({github, repository, head}) => {
     };
   });
 
-  // 差分コミットのshaと一致するPRに絞り込み、リリースPRで使用するマージされたPR一覧のテキストを作成する
+  return pullList;
+};
+
+// 差分コミットのshaと一致するPRに絞り込み、リリースPRで使用するマージされたPR一覧のテキストを作成する
+const createPullRequestBody = (pullsList, commits) => {
   // 注) release, hotfix からのPRは取り除く
   const ignoreBranches = ['release', 'hotfix'];
+  // リリースPRで使用するマージされたPR一覧のテキストを整形
+  const format = pr => {
+    const prHashNumber = `#${pr.number}`;
+    const regrex = /[0-9A-Za-z]+-[0-9]+/g;
+    const title = pr.title.replace(
+      regrex,
+      '[$&](https://www.google.com/search?q=$&)',
+    );
+
+    return `- ${prHashNumber} ${title}`;
+  };
+
+  console.log(
+    pullsList.filter(
+      pr => !ignoreBranches.some(branch => pr.head.ref.includes(branch)),
+    ),
+  );
+  console.log(
+    pullsList
+      .filter(
+        pr => !ignoreBranches.some(branch => pr.head.ref.includes(branch)),
+      )
+      .filter(pr => commits.some(commit => commit.sha === pr.merge_commit_sha)),
+  );
+
   const content = pullsList
     .filter(pr => !ignoreBranches.some(branch => pr.head.ref.includes(branch)))
     .filter(pr => commits.some(commit => commit.sha === pr.merge_commit_sha))
@@ -45,9 +90,11 @@ module.exports = async ({github, repository, head}) => {
   content.unshift(`## Changes`);
   const body = content.join('\n');
 
-  console.log(body)
+  return body;
+};
 
-  // PRをmaster, developに出す
+// PRをmaster, developに出す
+const createPullRequest = (github, {owner, repo, head, body, version}) => {
   // to master
   const resultPullsCreateToMaster = await github.pulls.create({
     owner,
@@ -58,9 +105,6 @@ module.exports = async ({github, repository, head}) => {
     body,
     draft: true,
   });
-
-  console.log(resultPullsCreateToMaster)
-
   // to develop
   await github.pulls.create({
     owner,
@@ -68,20 +112,7 @@ module.exports = async ({github, repository, head}) => {
     head,
     base: 'develop',
     title: `Merge release branch ${version} to develop`,
-    body: `## Relation\n#${resultPullsCreateToMaster.number}\n${body}`,
+    body: `## Relation\n#${resultPullsCreateToMaster.data.number}\n${body}`,
     draft: true,
   });
-};
-
-// リリースPRで使用するマージされたPR一覧のテキストを整形
-const format = pr => {
-  const prHashNumber = `#${pr.number}`;
-  const regrex = /[0-9A-Za-z]+-[0-9]+/g;
-
-  const title = pr.title.replace(
-    regrex,
-    '[$&](https://www.google.com/search?q=$&)',
-  );
-
-  return `- ${prHashNumber} ${title}`;
 };
